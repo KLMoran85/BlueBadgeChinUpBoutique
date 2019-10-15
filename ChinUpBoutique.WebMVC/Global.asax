49f0984405e3e844edcdb95a3721060f1ac65656
KLMoran85 <KLMoran85@gmail.com>
2019-10-15 13:28:57 -0400

    Add project files.

---
+++ ChinUpBoutique.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChinUpBoutique.WebMVC.MvcApplication" Language="C#" %>
---
+++ ChinUpBoutique.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChinUpBoutique.WebMVC.Startup))]
+namespace ChinUpBoutique.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
